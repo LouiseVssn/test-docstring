159417a99924b9de10df79666f988c7e41b851d2
LouiseVssn <126552016+LouiseVssn@users.noreply.github.com>
2023-03-15 18:11:06 +0100

    Mise à jour du code avec p + organisation.

--- devoir.py
+++ devoir.py
@@ -1,14 +1,22 @@
 import numpy as np
 import math as math
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 
-tau     =   2    # valeur taux compression [-]
-D       =   2    # valeur alesage           [m]
-C       =   3    # valeur course [m]
-L       =   4    # valeur longueur bielle@ [m]
-mpiston =   5   # valeur masse piston@ [kg]
-mbielle =   6   # valeur masse bielle  [kg]
-Q       =   1650   # valeur chaleur emise par fuel par kg de melange admis@ [J/kg_inlet gas]
+tau     =   10    # valeur taux compression [-]
+D       =   0.08    # valeur alesage   [m]
+C       =   0.09    # valeur course [m]
+L       =   0.18    # valeur longueur bielle@ [m]
+mpiston =   0.8   # valeur masse piston@ [kg]
+mbielle =   0.9   # valeur masse bielle  [kg]
+Q       =   2800000  # valeur chaleur emise par fuel par kg de melange admis@ [J/kg_inlet gas]
+
+R = C / 2 # Longueur de manivelle
+V_critique = (( math.pi * D**2) / 4 ) * (2 * R) # OK
+masse_air = 0.029 * V_critique/(8.31451*303.15) # OK
+beta = L / R # rapport entre la longueur de la bielle et la longueur de la manivelle
+V_min = (1 / ( tau - 1)) * V_critique
+V_max = (tau / (tau -1)) * V_critique
 
 ###############################
 #    Runge-Kutta (pour p)     #
@@ -30,6 +38,145 @@ def integrator(Xstart,Ustart,Xend,h,f):
         U[i+1,:] = U[i,:] + h*(K1+2*K2+2*K3+K4)/6     
     return X,U
 
+def vol (theta):
+    
+    """""""""""""""""
+    Calcul du V_output en fonction de thêta
+    
+    """""""""""""""""
+    V_output = np.zeros(theta.size)
+
+    for i in range (len(theta)):
+        V_output[i] = V_critique/2 * (1 - math.cos(theta[i]*math.pi/180) + beta - np.sqrt(beta**2 - math.sin(theta[i]*math.pi/180)**2)) + V_min
+        
+    """""""""""""""""
+    Graphe de V_output en fct de theta
+    
+    """""""""""""""""
+    plt.plot(theta, V_output/V_max)
+    
+    # Axes
+    fs_text = 16 # Taille du texte
+    plt.xlabel("$Thêta$ [degré]", fontsize=fs_text)
+    plt.ylabel("$V_{output}$ [m^3]", fontsize=fs_text)
+    
+    # Titre
+    plt.title("Volume/V_max en fonction de l'angle de vilebrequin", fontsize=fs_text)
+    plt.show()
+
+    return V_output
+
+def q(theta):
+    """""""""""""""""
+    Calcul du Q_output en fonction de thêta 
+
+    """""""""""""""""
+    Q_output = np.zeros(theta.size)
+    Q_max = float('-inf')
+    Q_min = float('inf')
+    for i in range (len(theta)):
+        if (theta[i] < thetaC or theta [i] > thetaC + deltaThetaC):
+            Q_output[i] = 0
+        
+        else:
+            fraction = (theta[i] + thetaC)/deltaThetaC # Pas besoin de mettre en radiant car fraction.
+            Q_output[i] = (Q *s* masse_air)/ 2 * (1 - math.cos(math.pi * fraction))
+            if (Q_output[i] > Q_max) :
+                Q_max = Q_output[i] 
+                print(i)
+            if (Q_output[i] < Q_min) :
+                Q_min = Q_output[i]
+    
+    
+    """""""""""""""""
+    Graphe de Q_output en fct de theta
+    
+    """""""""""""""""
+    plt.plot(theta, Q_output)
+    
+    # Axes
+    fs_text = 16 # Taille du texte
+    plt.xlabel("$Thêta$ [degré]", fontsize=fs_text)
+    plt.ylabel("$Q_{output}$ [J]", fontsize=fs_text)
+    
+    # Titre
+    plt.title("Q_max = " + str(Q_max) + "    Q_min = " + str(Q_min))
+    plt.suptitle("Chaleur en fonction de l'angle de vilebrequin", fontsize=fs_text)
+    plt.show()
+
+    return Q_output
+
+
+
+def pression(theta, s, thetaC, deltaThetaC) : 
+    """""""""""""""""
+    Calcul du p_output en fonction de thêta 
+
+    """""""""""""""""
+    def model(p,theta):
+
+        theta_rad = math.radians(theta)
+        racine = np.sqrt(beta*beta - (math.sin(theta_rad)*math.sin(theta_rad)))
+        V_formule = V_critique/2 * (1 - math.cos(theta_rad) + beta - racine) + V_min # Je peux pas prendre vokl(theta) var renvoie un array et j'ai besoin d'un float.
+        fraction = (theta-thetaC)/deltaThetaC
+        
+        dVdtheta = V_critique/2*(math.sin(theta_rad) + (math.sin(theta_rad)*math.cos(theta_rad))/(racine))
+        
+        dQdtheta = (math.pi*Q*masse_air*s)/(2*deltaThetaC) * (math.sin(math.pi*fraction))
+        
+        if (thetaC <= theta < thetaC + deltaThetaC) : # Si dans combustion
+            dpdt = - 1.3 *p/V_formule * dVdtheta + (1.3 - 1)/V_formule * dQdtheta
+        else :
+            dpdt = - 1.3 *p/V_formule * dVdtheta
+        
+        return dpdt
+ 
+    p_output = odeint(model,s,theta)
+    plt.plot(theta,p_output)
+    plt.title("Pression")
+    plt.show()
+     
+
+def F_tete(theta, p_output, w) : 
+    """""""""""""""""
+    Calcul du F_tete_output en fonction de thêta 
+
+    """""""""""""""""
+    F_tete_output = np.zeros(theta.size)
+    for k in range(len(theta)) : 
+        F_tete_output[k] = -(math.pi*D**2)/4 *p_output[k] - (mpiston + mbielle)*R*w**2*math.cos(math.radians(theta[k]))
+     
+            
+    plt.plot(theta, F_tete_output)
+    plt.title("F_tete_output")
+    plt.show()
+
+    return F_tete_output
+
+
+def F_pied(theta, p_output, w) : 
+    """""""""""""""""
+    Calcul du F_pied_output en fonction de thêta 
+
+    """""""""""""""""
+    F_pied_output = np.zeros(theta.size)
+    for j in range(len(theta)) : 
+        F_pied_output[j] = (math.pi*D**2)/4 *p_output[j] - mpiston*R*w**2*math.cos(math.radians(theta[j]))
+        
+    plt.plot(theta, F_pied_output)
+    plt.title("F_pied_output")
+    plt.show()
+
+    return F_pied_output
+
+
+def t() : 
+    """""""""""""""""
+    Calcul du t 
+
+    """""""""""""""""
+    pass
+        
 
 def myfunc(rpm, s, theta, thetaC, deltaThetaC):
     """ 
@@ -56,78 +203,34 @@ def myfunc(rpm, s, theta, thetaC, deltaThetaC):
     F_inertie(theta) : [N]
     (une force est positive si dirigée vers le haut).
     """
-	
-	
-    """""""""""""""""
-    Calcul du V_output en fonction de thêta
-    
-    """""""""""""""""
-    
-    V_output = np.zeros(theta.size)
-    print(theta.size)
-    print(V_output)
-    R = C / 2 # Longueur de manivelle
-    V_cylindree= (( math.pi * pow(D, 2)) / 4 ) * (2 * R)
-    beta = L / R # rapport entre la longueur de la bielle et la longueur de la manivelle
-    V_min = (1 / ( tau - 1)) * V_cylindree
-    V_max = V_min * tau
-    print(theta)
-    print(V_min)
-    print(V_max)
-    print(V_cylindree)
-    for i in range (len(theta)):
-        V_output[i] = (V_cylindree / 2) * (1 - math.cos(theta[i]) + beta - math.sqrt(pow(beta, 2) - pow(math.sin(theta [i]), 2))) + V_min 
-    
-    """""""""""""""""
-    Graphe de V_output en fct de theta
-    
-    """""""""""""""""
-    markerline, stemlines, baseline = plt.stem(theta, V_output/V_max)
-    
-    # Axes
-    fs_text = 16 # Taille du texte
-    plt.xlabel("$Thêta$ [degré]", fontsize=fs_text)
-    plt.ylabel("$V_output$ [m^3]", fontsize=fs_text)
-    
-    # Titre
-    plt.title("Volume disponible pour le gaz en fonction de l'angle de vilebrequin", fontsize=fs_text)
-    plt.show()
-    
 
-    """""""""""""""""
-    Calcul du Q_output en fonction de thêta 
+    w = rpm/60*2*math.pi # Transformation de revolution/minute en rad/s.    
 
-    """""""""""""""""
+    V_output = vol (theta)
+    Q_output = q(theta)
+    p_output = pression(theta, s, thetaC, deltaThetaC)
+    F_tete_output = F_tete(theta, p_output, w)
+    F_pied_output = F_pied(theta, p_output, w)
+    t = t()
     
-    """""""""""""""""
-    Calcul du F_pied_output en fonction de thêta 
+    #V_output = np.ones_like(theta)
+    #Q_output      = 2*np.ones_like(theta);
+    #F_pied_output = 3*np.ones_like(theta);
+    #F_tete_output = 4*np.ones_like(theta);
+    #p_output      = 5*np.ones_like(theta);
+    #t             = 6;
 
-    """""""""""""""""
-    
-     
-    """""""""""""""""
-    Calcul du F_tete_output en fonction de thêta 
+    return ( F_pied_output, F_tete_output, p_output, t  )
 
-    """""""""""""""""
-    
-     
-    """""""""""""""""
-    Calcul du p_output en fonction de thêta 
 
-    """""""""""""""""
-    
-     
-    """""""""""""""""
-    Calcul du t 
 
-    """""""""""""""""
-    
-    
-    Q_output      = 2*np.ones_like(theta);
-    F_pied_output = 3*np.ones_like(theta);
-    F_tete_output = 4*np.ones_like(theta);
-    p_output      = 5*np.ones_like(theta);
-    t             = 6;
-    
-    return V_output, Q_output, F_pied_output, F_tete_output, p_output, t  ;
+rpm = 2000
+s = 1.5
+Pin = s*1E5
+thetaC = 25
+deltaThetaC = 50
+theta = np.linspace(-180., 180., 100)
+
+#print(myfunc(2555,1.9,np.linspace(-1*180,180,10000), 26,43))
+print(myfunc(rpm, s, theta, thetaC, deltaThetaC))
 
